1f382e46327068ccbba7aef87df1316307d031dc
Mahendra Dwi Purwanto <mahendradwipurwanto@gmail.com>
2022-08-31 10:05:48 +0700

    captcha & validasi

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -16,7 +16,31 @@ class Register extends CI_Controller
             $this->session->set_flashdata('warning', 'Berhasil masuk ke akun.');
             redirect(site_url('home'));
         } else {
-            $this->templateauth->view('authentication/register');
+            $vals = [
+                // 'word' -> nantinya akan digunakan sebagai random teks yang akan keluar di captchanya
+                'word'          => substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 8),
+                'img_path'      => './berkas/captcha/',
+                'img_url'       => base_url('berkas/captcha/'),
+                'img_width'     => 150,
+                'img_height'    => 30,
+                'expiration'    => 7200,
+                'word_length'   => 8,
+                'font_size'     => 16,
+                'img_id'        => 'Imageid',
+                'pool'          => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                'colors'        => [
+                        'background'=> [255, 255, 255],
+                        'border'    => [255, 255, 255],
+                        'text'      => [0, 0, 0],
+                        'grid'      => [255, 40, 40]
+                ]
+            ];
+            
+            $captcha = create_captcha($vals);
+            // ej($captcha);
+            $this->session->set_userdata('captcha', $captcha['word']);
+            $data['captcha'] = $captcha['image'];
+            $this->templateauth->view('authentication/register', $data);
         }
     }
   
@@ -28,54 +52,63 @@ class Register extends CI_Controller
         $email          = htmlspecialchars($this->input->post('email'));
         $password       = htmlspecialchars($this->input->post('password'));
         $password_conf  = htmlspecialchars($this->input->post('password_conf'));
-    
-        // cek apakah email telah ada
-        if ($this->M_auth->cek_user($email) == false) {
-      
-            // cek apakah password sama
-            if ($password == $password_conf) {
+        $post_code      = $this->input->post('captcha');
+        $captcha        = $this->session->userdata('captcha');
         
-                 // ubah inputan view menjadi array
-                $data_user = array(
-                    'email'     => $email,
-                    'aktivasi'  => $this->M_auth->create_kode(),
-                    'password'  => password_hash($password, PASSWORD_DEFAULT),
-                    'created_at' => time()
-                );
+        // cek captcha
+        if($post_code && ($post_code == $captcha)){
+            // ej($captcha);
+            // cek apakah email telah ada
+            if ($this->M_auth->cek_user($email) == false) {
         
-                // masukkan ke database
-                if ($this->M_auth->add_user($data_user) == true) {
-                    $subject    = "Selamat bergabung - {$email}";
-                    $message    = "Hai, {$nama} selamat bergabung.</br></br></br></br>";
-          
-                    $this->send_email($email, $subject, $message);
-
-                    $user     = $this->M_auth->get_user($email);
-                
-                    // simpan data user yang login kedalam session
-                    $session_data = array(
-                        'user_id'   => $user->user_id,
-                        'nama'      => $user->nama,
-                        'email'     => $user->email,
-                        'no_telp'   => $user->no_telp,
-                        'role'      => $user->role,
-                        'logged_in' => true,
+                // cek apakah password sama
+                if ($password == $password_conf) {
+            
+                    // ubah inputan view menjadi array
+                    $data_user = array(
+                        'email'     => $email,
+                        'aktivasi'  => $this->M_auth->create_kode(),
+                        'password'  => password_hash($password, PASSWORD_DEFAULT),
+                        'created_at' => time()
                     );
-          
-                    $this->session->set_userdata($session_data);
-          
-                    $this->session->set_flashdata('success', "Berhasil mendaftaran akun Anda. Harap melanjutkan proses aktivasi!");
-                    redirect(site_url('aktivasi-akun'));
+            
+                    // masukkan ke database
+                    if ($this->M_auth->add_user($data_user) == true) {
+                        $subject    = "Selamat bergabung - {$email}";
+                        $message    = "Hai, {$nama} selamat bergabung.</br></br></br></br>";
+            
+                        $this->send_email($email, $subject, $message);
+
+                        $user     = $this->M_auth->get_user($email);
+                    
+                        // simpan data user yang login kedalam session
+                        $session_data = array(
+                            'user_id'   => $user->user_id,
+                            'nama'      => $user->nama,
+                            'email'     => $user->email,
+                            'no_telp'   => $user->no_telp,
+                            'role'      => $user->role,
+                            'logged_in' => true,
+                        );
+            
+                        $this->session->set_userdata($session_data);
+            
+                        $this->session->set_flashdata('success', "Berhasil mendaftaran akun Anda. Harap melanjutkan proses aktivasi!");
+                        redirect(site_url('aktivasi-akun'));
+                    } else {
+                        $this->session->set_flashdata('error', "Terjadi kesalahan saat mendaftarkan akun Anda. Harap coba lagi!");
+                        redirect(site_url('register'));
+                    }
                 } else {
-                    $this->session->set_flashdata('error', "Terjadi kesalahan saat mendaftarkan akun Anda. Harap coba lagi!");
+                    $this->session->set_flashdata('warning', "Password yang Anda masukkan tidak sama!");
                     redirect(site_url('register'));
                 }
             } else {
-                $this->session->set_flashdata('warning', "Password yang Anda masukkan tidak sama!");
+                $this->session->set_flashdata('warning', "Email atau username telah digunakan !");
                 redirect(site_url('register'));
             }
-        } else {
-            $this->session->set_flashdata('warning', "Email atau username telah digunakan !");
+        }else{
+            $this->session->set_flashdata('warning', "Captcha yang anda masukkan salah !");
             redirect(site_url('register'));
         }
     }
--- application/views/authentication/register.php
+++ application/views/authentication/register.php
@@ -13,13 +13,13 @@
 
                 <div class="form-group row">
                     <div class="col-12">
-                        <input class="form-control" type="text" name="nama" required placeholder="Nama lengkap">
+                        <input class="form-control" type="text" name="nama" required placeholder="Nama lengkap" id="signinSrNama">
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <div class="col-12">
-                        <input class="form-control" type="text" name="no_telp" required placeholder="Nomor telepon">
+                        <input class="form-control" type="text" name="no_telp" required placeholder="Nomor telepon" id="signinSrTelepon">
                     </div>
                 </div>
 
@@ -47,6 +47,13 @@
                     </div>
                 </div>
 
+                <div class="form-group row">
+                    <div class="col-12">
+                        <?=$captcha?>
+                        <input class="form-control" type="text" name="captcha" required placeholder="Masukkan captcha">
+                    </div>
+                </div>
+
                 <div class="form-group text-center row m-t-20">
                     <div class="col-12">
                         <button class="btn btn-danger btn-block waves-effect waves-light" type="submit" id="send-button">Daftar</button>
@@ -63,3 +70,42 @@
 
     </div>
 </div>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    $("#signinSrNama").keydown(function(event){
+      var inputValue = event.which;
+        // allow letters and whitespaces only.
+        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0 && inputValue != 8 && inputValue != 37 && inputValue != 39)) { 
+          event.preventDefault(); 
+        }
+      });
+  });
+  
+  $("#signinSrTelepon").keyup(function(){
+    var value = $(this).val();
+    value = value.replace(/^(0*)/,"");
+    $(this).val(value);
+  });
+
+  // Restricts input for the given textbox to the given inputFilter.
+  function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      textbox.addEventListener(event, function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          this.value = "";
+        }
+      });
+    });
+  }
+
+  // Install input filters Tambah Hp Pegawai.
+  setInputFilter(document.getElementById("signinSrTelepon"), function(value) { return /^\d*$/.test(value); });
+</script>
